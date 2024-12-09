274743b18fd36c023a9281e00b2ba523c9537624
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-12-09 21:19:39 +0200

    jpg -> webp

--- src/components/home/home.data.ts
+++ src/components/home/home.data.ts
@@ -2,22 +2,22 @@ import type { ISlide } from './home.types'
 
 export const SLIDES: ISlide[] = [
 	{
-		bgImg: '/uploads/1.jpg',
+		bgImg: '/uploads/1.webp',
 		title: 'Трекові магнітні світильники 48V',
 		url: '/',
 	},
 	{
-		bgImg: '/uploads/2.jpg',
+		bgImg: '/uploads/2.webp',
 		title: 'Магнітні шини під гіпсокартон',
 		url: '/',
 	},
 	{
-		bgImg: '/uploads/3.jpg',
+		bgImg: '/uploads/3.webp',
 		title: 'Трековий магнітний світильник подвійний',
 		url: '/',
 	},
 	{
-		bgImg: '/uploads/4.jpg',
+		bgImg: '/uploads/4.webp',
 		title: 'Трековий магнітний світильник подвійний',
 		url: '/',
 	}
