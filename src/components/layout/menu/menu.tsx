a91c3f74cf28a3d6aa7cb0f8c99d7e8e64a020e6
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-12-13 19:59:03 +0200

    fix

--- src/components/layout/menu/menu.tsx
+++ src/components/layout/menu/menu.tsx
@@ -6,13 +6,23 @@ import { categoriesService } from '@/app/services/categories.service'
 export const revalidate = 180
 
 export async function Menu() {
-	const categories: Category[] = (await categoriesService.getAllCategories()).data
+	try {
+		const categories: Category[] = (await categoriesService.getAllCategories()).data
 
-	return (
-		<div className='w-full h-full text-foreground'>
-			<Accordion title='Категорії'>
-				<MenuList categories={categories} />
-			</Accordion>
-		</div>
-	)
+		return (
+			<div className='w-full h-full text-foreground'>
+				<Accordion title='Категорії'>
+					<MenuList categories={categories} />
+				</Accordion>
+			</div>
+		)
+	} catch (error) {
+		return (
+			<div className='w-full h-full text-foreground'>
+				<Accordion title='Категорії'>
+					<div>Не вдалося завантажити категорії, спробуйте оновити сторінку</div>
+				</Accordion>
+			</div>
+		)
+	}
 }
