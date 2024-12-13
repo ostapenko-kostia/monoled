bcc48f9e1d12789e5572036bdaa92aff0555782d
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-12-13 20:11:44 +0200

    fix

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Monoled - Щось пішло не так...'
+}
+
+export default function Page() {
+	return <div>Упс... Щось пішло не так</div>
+}
--- src/components/layout/menu/menu.tsx
+++ src/components/layout/menu/menu.tsx
@@ -6,23 +6,12 @@ import { categoriesService } from '@/app/services/categories.service'
 export const revalidate = 180
 
 export async function Menu() {
-	try {
-		const categories: Category[] = (await categoriesService.getAllCategories()).data
-
-		return (
-			<div className='w-full h-full text-foreground'>
-				<Accordion title='Категорії'>
-					<MenuList categories={categories} />
-				</Accordion>
-			</div>
-		)
-	} catch (error) {
-		return (
-			<div className='w-full h-full text-foreground'>
-				<Accordion title='Категорії'>
-					<div>Не вдалося завантажити категорії, спробуйте оновити сторінку</div>
-				</Accordion>
-			</div>
-		)
-	}
+	const categories: Category[] = (await categoriesService.getAllCategories()).data
+	return (
+		<div className='w-full h-full text-foreground'>
+			<Accordion title='Категорії'>
+				<MenuList categories={categories} />
+			</Accordion>
+		</div>
+	)
 }
