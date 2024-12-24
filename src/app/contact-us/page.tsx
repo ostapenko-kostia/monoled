f2a8e1eb52ff4a84c350bf02d95428aab010b08b
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-12-24 13:34:55 +0200

    remove second footer in about page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -5,11 +5,11 @@ import Link from 'next/link'
 export default function ContactUsPage() {
 	return (
 		<>
-			<header className='flex items-center justify-center py-36 w-full gap-2 flex-col relative text-white'>
+			<header className="flex items-center justify-center py-36 w-full gap-2 flex-col relative text-white">
 				<div
-					className='absolute top-0 left-0 inset-0 w-full h-full -z-50 brightness-[.30]'
+					className="absolute top-0 left-0 inset-0 w-full h-full -z-50 brightness-[.30]"
 					style={{
-						backgroundImage: "url('/1.avif')",
+						backgroundImage: 'url(\'/1.avif\')',
 						backgroundAttachment: 'fixed',
 						backgroundPosition: 'center 70%',
 						backgroundRepeat: 'no-repeat',
@@ -18,22 +18,22 @@ export default function ContactUsPage() {
 					}}
 				/>
 
-				<h2 className='font-medium text-5xl max-[450px]:text-4xl'>Контакти</h2>
-				<p className='text-lg font-semibold max-[450px]:text-base'>
+				<h2 className="font-medium text-5xl max-[450px]:text-4xl">Контакти</h2>
+				<p className="text-lg font-semibold max-[450px]:text-base">
 					<Link
-						className='underline-offset-[6px] hover:underline'
-						href='/'
+						className="underline-offset-[6px] hover:underline"
+						href="/"
 					>
 						Домівка
 					</Link>
 					{' > '}
-					<span className='font-normal'>Контакти</span>
+					<span className="font-normal">Контакти</span>
 				</p>
 			</header>
-			<div className='grid-cols-2 grid gap-10 py-10 max-sm:grid-cols-1'>
-				<div className='my-20 container mx-auto max-sm:px-2 min-h-screen'>
-					<h1 className='text-5xl mb-10 text-center uppercase font-semibold'>Monoled</h1>
-					<p className='mb-10 text-center mx-auto w-[90%]'>
+			<div className="grid-cols-2 grid gap-10 py-10 max-sm:grid-cols-1">
+				<div className="my-20 container mx-auto max-sm:px-2 min-h-screen">
+					<h1 className="text-5xl mb-10 text-center uppercase font-semibold">Monoled</h1>
+					<p className="mb-10 text-center mx-auto w-[90%]">
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, reprehenderit iure
 						assumenda incidunt commodi eaque dolorem excepturi ad officiis sint molestias quo atque
 						error sunt architecto dolor ratione consectetur consequatur? Laboriosam ea velit rerum
@@ -63,19 +63,19 @@ export default function ContactUsPage() {
 						facere sint ut qui mollitia, eius cupiditate sapiente.
 					</p>
 				</div>
-				<div className='w-full h-min aspect-square sticky top-5 max-sm:hidden'>
-					<div className='relative w-full h-full'>
+				<div className="w-full h-min aspect-square sticky top-5 max-sm:hidden">
+					<div className="relative w-full h-full">
 						<Image
-							src='/1.avif'
-							alt='about'
-							className='object-cover'
+							src="/1.avif"
+							alt="about"
+							className="object-cover"
 							fill
-							sizes='100%, 100%'
+							sizes="100%, 100%"
 						/>
 					</div>
 				</div>
 			</div>
-			<div className='py-20 bg-neutral-100'>
+			<div className="py-20 bg-neutral-100">
 				<ContactUsForm />
 			</div>
 		</>
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,10 +8,13 @@ interface Props {
 export function Footer({ className }: Props) {
 	return (
 		<footer
-			className={cn(className, 'bg-foreground text-background px-8 py-5 max-sm:px-4 w-ful h-full flex flex-col items-center text-center gap-4')}
+			className={cn(
+				className,
+				'bg-foreground text-background px-8 py-5 max-sm:px-4 w-ful h-full flex flex-col items-center text-center gap-4'
+			)}
 		>
 			<Logo />
-			<span className="text-neutral-400">Copyright 2024. All rights reserved</span>
+			<span className='text-neutral-400'>Copyright 2024. All rights reserved</span>
 		</footer>
 	)
 }
