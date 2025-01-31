15e47cf6a5c8356d59d16afde087c3120c3ad0ea
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-01-31 17:02:42 +0200

    optimization

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,22 +1,18 @@
-'use client'
-
 import Link from 'next/link'
 import localFont from 'next/font/local'
 import clsx from 'clsx'
-import { useTexts } from '@/context/textContext'
+import { textsService } from '@/services/texts.service'
 
 const bestari = localFont({
 	src: [{ path: '../../fonts/Quantify-Bold.woff2', weight: '700', style: 'normal' }]
 })
 
-function Logo() {
-	const title = useTexts()?.find(text => text.slug === 'title')?.text
+async function Logo() {
+	const title = (await textsService.getAllTexts())?.find(text => text.slug === 'title')?.text
 
 	return (
 		<Link href='/'>
-			<span className={clsx('text-4xl tracking-[0.07em]', bestari.className)}>
-				{title ?? ''}
-			</span>
+			<span className={clsx('text-4xl tracking-[0.07em]', bestari.className)}>{title ?? ''}</span>
 		</Link>
 	)
 }
