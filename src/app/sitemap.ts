b469334873b7c89aa2e3371e88bf2f2be420dd6f
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-02-03 12:59:10 +0200

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,50 +1,35 @@
 import { productsService } from '@/services/products.service'
 import { ProductWithInfo } from '@/typing/interfaces'
-import { NextResponse } from 'next/server'
+import { MetadataRoute } from 'next'
 
-export async function GET() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const products: ProductWithInfo[] | undefined = (await productsService.getAllProducts())?.data
 
-	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://lumineka.com.ua/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>1.00</priority>
-    </url>
-    <url>
-      <loc>https://lumineka.com.ua/product/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>0.80</priority>
-    </url>
-    <url>
-      <loc>https://lumineka.com.ua/shop</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>0.80</priority>
-    </url>
-    <url>
-      <loc>https://lumineka.com.ua/about</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>0.70</priority>
-    </url>
-    <url>
-      <loc>https://lumineka.com.ua/contact-us</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>0.70</priority>
-    </url>
-    ${products
-			?.map(
-				product => `
-    <url>
-      <loc>https://lumineka.com.ua/product/${product.slug}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>0.80</priority>
-    </url>`
-			)
-			.join('')}
-  </urlset>`
-
-	return NextResponse.json(sitemap, {
-		headers: { 'Content-Type': 'application/xml' }
-	})
+	return [
+		{
+			url: 'https://lumineka.com.ua/',
+			lastModified: new Date(),
+			priority: 1.0
+		},
+		{
+			url: 'https://lumineka.com.ua/shop',
+			lastModified: new Date(),
+			priority: 0.8
+		},
+		{
+			url: 'https://lumineka.com.ua/about',
+			lastModified: new Date(),
+			priority: 0.7
+		},
+		{
+			url: 'https://lumineka.com.ua/contact-us',
+			lastModified: new Date(),
+			priority: 0.7
+		},
+		...(products?.map(product => ({
+			url: `https://lumineka.com.ua/product/${product.slug}`,
+			lastModified: new Date(),
+			priority: 0.8
+		})) || [])
+	]
 }
