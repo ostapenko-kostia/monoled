a704563ad52a64c51f79129dcb7008c454f4764b
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-03-24 18:34:47 +0200

    fix

--- src/app/admin/_components/admin-products/admin-product-edit/index.ts
+++ src/app/admin/_components/admin-products/admin-product-edit/index.ts
@@ -1,5 +1,3 @@
-export { AdminProductEditPrice } from './admin-product-edit-price'
-export { AdminProductEditQuantity } from './admin-product-edit-quantity'
 export { AdminProductEditName } from './admin-product-edit-name'
 export { AdminProductEditCategory } from './admin-product-edit-category'
 export { AdminProductEditModel } from './admin-product-edit-model'
